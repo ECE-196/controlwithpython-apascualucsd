df156ee7dc9a07b383603be1ec3c572ad995f25a
apascualucsd <102634499+apascualucsd@users.noreply.github.com>
2024-04-30 15:52:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from __future__ import annotations
-from threading import Thread, Lock
-from serial import Serial, SerialException
+from serial import Serial
+from threading import Thread, Lock 
 from serial.tools.list_ports import comports
+from serial import Serial, SerialException
 
 import tkinter as tk
 import tkinter.ttk as ttk
@@ -15,87 +15,88 @@ def detached_callback(f):
 
 class LockedSerial(Serial):
     _lock: Lock = Lock()
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
     def read(self, size=1) -> bytes:
         with self._lock:
             return super().read(size)
-        
+
     def write(self, b: bytes, /) -> int | None:
         with self._lock:
             super().write(b)
-            
+
     def close(self):
         with self._lock:
             super().close()
 
+class App(tk.Tk):
+    ser: LockedSerial
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("LED Blinker")
+
+        self.port = tk.StringVar()
+        self.led = tk.BooleanVar()
+
+        ttk.Checkbutton(self, text='Toggle LED', variable=self.led, command=self.update_led).pack()
+        ttk.Button(self, text='Send Invalid', command = self.send_invalid).pack()
+        ttk.Button(self, text='Disconnect', command = self.disconnect, default='active').pack()
+        
+        SerialPortal(self)
+
+    @detached_callback
+    def connect(self):
+        self.ser = Serial(self.port.get())
+
+    @detached_callback
+    def disconnect(self):
+        self.ser.close()
+    
+        SerialPortal(self) # display portal to reconnect
+
+    @detached_callback
+    def update_led(self):
+        self.write(bytes([self.led.get()]))
+
+    @detached_callback
+    def send_invalid(self):
+        self.write(bytes([0x10]))
+
+    def write(self, b: bytes):
+        try:
+            self.ser.write(b)
+            if self.ser.read() == bytes([0xff]):
+                print('Error')
+                showerror('Device Error', 'The device reported an invalid command.')
+        except SerialException:
+            showerror('Serial Error', 'Write failed.')
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self.disconnect()
+
 class SerialPortal(tk.Toplevel):
     def __init__(self, parent: App):
         super().__init__(parent)
-        
+
         self.parent = parent
-        self.parent.withdraw()
-        
+        self.parent.withdraw() # hide App until connected
+
         ttk.OptionMenu(self, parent.port, '', *[d.device for d in comports()]).pack()
         ttk.Button(self, text='Connect', command=self.connect, default='active').pack()
-        
+
     @detached_callback
     def connect(self):
         self.parent.connect()
         self.destroy()
-        self.parent.deiconify()
+        self.parent.deiconify() # reveal App
 
-class App(tk.Tk):
-    ser: LockedSerial
-    
-    def __init__(self):
-        super().__init__()
-        
-        self.title("LED Blinker")
-        
-        self.port = tk.StringVar()
-        self.led = tk.BooleanVar()
-        
-        ttk.Checkbutton(self, text='Toggle LED', variable=self.led, command=self.update_led).pack()
-        ttk.Button(self, text='Send Invalid', command=self.send_invalid).pack()
-        ttk.Button(self, text='Disconnect', command=self.disconnect, default='active').pack()
-        
-        SerialPortal(self)
-        
-    @detached_callback
-    def connect(self):
-        self.ser = LockedSerial(self.port.get())
-    
-    @detached_callback
-    def disconnect(self):
-        self.ser.close()
-            
-        SerialPortal(self)
-            
-    @detached_callback
-    def update_led(self):
-        self.write(bytes([self.led.get()]))
-                
-    @detached_callback
-    def send_invalid(self):
-        self.write(bytes([0x10]))
-        
-    def write(self, b: bytes):
-        try:
-            self.ser.write(b)
-            if int.from_bytes(self.ser.read(), 'big') == S_ERR:
-                showerror('Device Error', 'The device reported an invalid command.')
-        except SerialException:
-            showerror('Serial Error', 'Write failed.')
-        
-    def __enter__(self):
-        return self
-    
-    def __exit__(self, *_):
-        self.disconnect()
-        
 if __name__ == '__main__':
     with App() as app:
         app.mainloop()
